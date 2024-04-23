ba00e6f6da9908841c5f35ac749f119d62b876c8
Deepfried-Chips <specificallydeepfriedchips@gmail.com>
2024-04-23 15:34:51 +0000

    Initial flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Uhhhhhh";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+    ];
+  in {
+
+    nixosConfigurations = import ./systems {inherit self;};
+
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alejandra
+
+            (writeShellApplication {
+              name = "update";
+              text = ''
+                nix flake update && git commit flake.lock -m "update flake lockfile"
+              '';
+            })
+          ];
+        };
+      }
+    );
+
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
+}
