3e700a98696283e88cd96a2907e7d7f7a2ed769f
Deepfried-Chips <specificallydeepfriedchips@gmail.com>
2024-04-23 17:02:01 +0000

    initial nix config

--- systems/common.nix
+++ systems/common.nix
@@ -98,6 +98,20 @@
     options = "--delete-older-than 30d";
   };
 
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+  };
+
+
   networking.networkmanager.enable = true;
 
   boot.supportedFilesystems = ["exfat" "vfat" "ntfs" "ext" "btrfs" "xfs"];
