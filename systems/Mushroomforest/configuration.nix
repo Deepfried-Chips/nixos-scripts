2ad226b7b5815a54c729991b644ed0c9f20d3391
Deepfried-Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2024-05-31 10:47:23 +0300

    oops

--- systems/Mushroomforest/configuration.nix
+++ systems/Mushroomforest/configuration.nix
@@ -14,7 +14,7 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = lib.mkForce false;;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.verbose = false;
   boot.consoleLogLevel = 0;
